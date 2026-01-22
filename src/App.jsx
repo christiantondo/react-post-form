f370ff25b480da01f0d12984bb9a4045f0a345e5
christiantondo <christiantondo1@gmail.com>
2026-01-22 23:44:02 +0100

    basic html

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,18 @@ function App() {
 	return (
 		<>
 			<h1>hello react world</h1>
+			<ul>
+				<li>author: </li>
+				<li>title: </li>
+				<li>body: </li>
+				<li>isPublic: </li>
+			</ul>
+			<form>
+				<input />
+				<input />
+				<input />
+				<input />
+			</form>
 		</>
 	)
 }
