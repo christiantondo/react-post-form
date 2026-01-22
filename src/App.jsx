7295b36572199978ef77e046f4ad94a8e9ebd872
christiantondo <christiantondo1@gmail.com>
2026-01-22 23:52:05 +0100

    list and placeholder update

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
 import { useState } from 'react'
 
+const postIniziale = {
+	author: "",
+	title: "",
+	body: "",
+	isPublic: true
+};
+
+
 function App() {
 
-	const postIniziale = {
-		author: "",
-		title: "",
-		body: "",
-		isPublic: true
-	};
+	const [post, setPost] = useState(postIniziale)
 
 	return (
 		<>
 			<h1>hello react world</h1>
 			<ul>
-				<li>author: </li>
-				<li>title: </li>
-				<li>body: </li>
-				<li>isPublic: </li>
+				<li>author: {post.author}</li>
+				<li>title: {post.title}</li>
+				<li>body: {post.body}</li>
+				<li>isPublic: {post.isPublic}</li>
 			</ul>
 			<form>
-				<input />
-				<input />
-				<input />
-				<input />
+				<input name="author" placeholder="post.author" value="" />
+				<input name="title" placeholder="post.title" value="" />
+				<input name="body" placeholder="post.body" value="" />
+				<input name="isPublic" placeholder="post.isPublic" value="" />
 			</form>
 		</>
 	)
